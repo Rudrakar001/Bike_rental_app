3e3a2a4dd4c53fee2aa4d488d947e255540bcc32
Rudra Kar <rudrakar2143@gmail.com>
2025-06-14 20:41:56 +0530

    deletion

--- server/Database/pool.js
+++ server/Database/pool.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
   database: 'Bikerent',
-  password:'root',
+  password:'Rudra@2002',
   waitForConnections: true,
   connectionLimit: 10,
   maxIdle: 10
